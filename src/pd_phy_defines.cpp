a0927ffa892e2bfa9db5a34887082a3319401714
mickdassive <adenandreana@gmail.com>
2024-02-28 14:58:27 -0500

    begain work on usbpd implimentation

---
+++ src/pd_phy_defines.cpp
@@ -0,0 +1,24 @@
+//{usbc car module}
+//{main.cpp}
+//Copyright (C) {2023}  {mickmake}
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//usbc car module
+//15/09/2023
+//Tokyo Andreana
+
+#include <Arduino.h>
+
+//phy address and register defines
+const uint8_t phy_add_ufp = 0xE4;
+const uint8_t phy_add_dfp = 0xE0;
