5e1e8bcbe5347c03a1b1408de00646591f5d67d6
mickdassive <adenandreana@gmail.com>
2024-05-30 20:54:37 -0400

    working on wifi

---
+++ src/wifi.h
@@ -0,0 +1,29 @@
+//{usbc car module}
+//{wifi.h}
+//Copyright (C) {2023}  {mickmake}
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//usbc car module
+//15/09/2023
+//Tokyo Andreana
+
+#ifndef wifi_h
+#define wifi_h
+
+//config vars
+const char* wifi_ssid = "xxxxxxxxxx";
+const char* wifi_password = "xxxxxxxxxx";
+const int wifi_wait_time = 1000; 
+
+
+#endif // wifi_h
